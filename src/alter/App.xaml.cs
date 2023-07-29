5322ef8ffe3a801fcc377a33f603d9e212e213ee
Takeshi Katano <takatano@outlook.com>
2023-07-30 04:33:42 +0900

    Add services configuration

--- src/alter/App.xaml.cs
+++ src/alter/App.xaml.cs
@@ -1,17 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using AlterApp.ViewModels;
 
 namespace AlterApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public sealed partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use.
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configure the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModels
+            services.AddTransient<MainWindowViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
