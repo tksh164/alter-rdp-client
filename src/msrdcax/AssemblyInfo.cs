b6c345cd4f63258c26a38e9622876e47e12a931c
Takeshi Katano <takatano@outlook.com>
2023-08-21 23:47:59 +0900

    Add version info to project file

--- src/msrdcax/AssemblyInfo.cs
+++ src/msrdcax/AssemblyInfo.cs
@@ -1,26 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Remote Desktop Services ActiveX client for Alter")]
-[assembly: AssemblyDescription("Remote Desktop Services ActiveX client for Alter.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("msrdcax")]
-[assembly: AssemblyCopyright("Copyright (c) 2023-present Takeshi Katano. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df403af2-a5fb-4fcc-8efa-f97f437befd3")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -28,5 +8,25 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+//[assembly: AssemblyVersion("0.1.0.0")]
+//[assembly: AssemblyFileVersion("0.1.0.0")]
+
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("Remote Desktop Services ActiveX client for Alter")]
+//[assembly: AssemblyDescription("Remote Desktop Services ActiveX client for Alter.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("msrdcax")]
+//[assembly: AssemblyCopyright("Copyright (c) 2023 Takeshi Katano. All rights reserved.")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df403af2-a5fb-4fcc-8efa-f97f437befd3")]
