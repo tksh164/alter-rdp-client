7f79189bf48cd543a4553d30313b7aaf675702cc
Takeshi Katano <takatano@outlook.com>
2023-10-26 00:25:27 +0900

    Add app level default values

---
+++ src/alter/Models/AppDefaultValues.cs
@@ -0,0 +1,13 @@
+﻿namespace AlterApp.Models
+{
+    internal static class AppDefaultValues
+    {
+        public static double MainWindowMinWidth = 780;
+        public static double MainWindowMinHeight = 520;
+
+        public static double MainWindowWidth = 1024;
+        public static double MainWindowHeight = 768;
+
+        public static uint RdpPort = 3389;
+    }
+}
