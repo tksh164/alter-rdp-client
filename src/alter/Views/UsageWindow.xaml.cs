925c24c3e614e62c8cc35240ff908c7c946553d3
Takeshi Katano <takatano@outlook.com>
2024-05-26 00:55:52 +0900

    Add UsageWindow

---
+++ src/alter/Views/UsageWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AlterApp.Views
+{
+    /// <summary>
+    /// Interaction logic for UsageWindow.xaml
+    /// </summary>
+    public partial class UsageWindow : Window
+    {
+        public UsageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
